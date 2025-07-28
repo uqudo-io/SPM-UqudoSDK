4b137090c9004851f6a3c859e24e838c18700971
Mario Casola <m.casola@uqu.do>
2025-07-28 12:30:12 +0700

    Updated SDK to version 3.5.1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UqudoSDK",
-            url: "https://rm.dev.uqudo.io/repository/uqudo-public/io/uqudo/sdk/iOS-Uqudo/3.5.0/iOS-Uqudo-3.5.0.zip",
-            checksum: "2ed463cfc1774745ada416cf88c9dc3098513f6656da7b1d3291d588b365cb63"
+            url: "https://rm.dev.uqudo.io/repository/uqudo-public/io/uqudo/sdk/iOS-Uqudo/3.5.1/iOS-Uqudo-3.5.1.zip",
+            checksum: "9656f739706e65636c5f261f92e6ce8f199e6a1370f2401b9f7bc0c81203dfd7"
         )
     ]
 )
